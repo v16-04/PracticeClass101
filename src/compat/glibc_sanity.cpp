6bf7cae32c1b51a7677fa2e559c93bc4544e7faa
v16.04 <47376168+v16-04@users.noreply.github.com>
2019-03-06 00:08:43 -0800

    Add files via upload

--- src/compat/glibc_sanity.cpp
+++ src/compat/glibc_sanity.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/LeisureCoin-config.h"
+#include "config/PRACTICE1-config.h"
 #endif
 
 #include <cstddef>
