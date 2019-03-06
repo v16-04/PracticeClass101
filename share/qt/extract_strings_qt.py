5f18013579c528dfc07496e21bdb3620324a3866
v16.04 <47376168+v16-04@users.noreply.github.com>
2019-03-06 00:00:56 -0800

    Add files via upload

--- share/qt/extract_strings_qt.py
+++ share/qt/extract_strings_qt.py
@@ -10,7 +10,7 @@ import operator
 import os
 import sys
 
-OUT_CPP="qt/LeisureCoinstrings.cpp"
+OUT_CPP="qt/PRACTICE1trings.cpp"
 EMPTY=['""']
 
 def parse_po(text):
@@ -74,10 +74,10 @@ f.write("""
 #define UNUSED
 #endif
 """)
-f.write('static const char UNUSED *LeisureCoin_strings[] = {\n')
+f.write('static const char UNUSED *PRACTICE1_strings[] = {\n')
 messages.sort(key=operator.itemgetter(0))
 for (msgid, msgstr) in messages:
     if msgid != EMPTY:
-        f.write('QT_TRANSLATE_NOOP("LeisureCoin-core", %s),\n' % ('\n'.join(msgid)))
+        f.write('QT_TRANSLATE_NOOP("PRACTICE1-core", %s),\n' % ('\n'.join(msgid)))
 f.write('};\n')
 f.close()
